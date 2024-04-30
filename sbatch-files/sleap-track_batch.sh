c2d76c1fe9e506bfee932c46f64b275ddccc52cf
Michael Winding <michael.winding@crick.ac.uk>
2024-04-30 12:26:10 +0100

    combine into one script

--- sbatch-files/sleap-track_batch.sh
+++ sbatch-files/sleap-track_batch.sh
@@ -21,5 +21,38 @@ else
     echo "Submitting $num_files jobs for processing."
 fi
 
-# Submit the job with the environment variables and job array configuration
-sbatch --export=DIR="$DIR",CENTROID="$CENTROID",CEN_INS="$CEN_INS",NUM_FILES=$num_files sleap-track_array-job.sh
+# Create SBATCH script dynamically
+echo "#!/bin/bash
+#SBATCH --job-name=sleap-infer
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --array=1-$num_files
+#SBATCH --partition=ncpu
+#SBATCH --mem=10G
+#SBATCH --time=08:00:00
+#SBATCH --mail-user=$(whoami)@crick.ac.uk
+#SBATCH --mail-type=FAIL
+
+ml purge
+ml Anaconda3/2023.09-0
+source /camp/apps/eb/software/Anaconda/conda.env.sh
+
+conda activate sleap
+
+DIR=$DIR
+CENTROID=$CENTROID
+CEN_INS=$CEN_INS
+
+FILE_PATH=\$(find \"${DIR}\" -maxdepth 1 -type f -name '*.mp4' | sort | sed -n \"\${SLURM_ARRAY_TASK_ID}p\")
+
+echo \"Processing file \$FILE_PATH\"
+NAME_VAR=\$(basename \"\$FILE_PATH\" .mp4)
+sleap-track \"\$FILE_PATH\" -m \$CENTROID -m \$CEN_INS -o \$DIR/\$NAME_VAR.predictions.slp
+sleap-convert \$DIR/\$NAME_VAR.predictions.slp -o \$DIR/\$NAME_VAR.json --format json
+sleap-convert \$DIR/\$NAME_VAR.predictions.slp -o \$DIR/\\$NAME_VAR.csv --format csv" > sleap-track_array-job.sh
+
+# Submit the dynamically created SBATCH script
+sbatch sleap-track_array-job.sh
+
+# remove the SBATCH script
+rm sleap-track_array-job.sh
