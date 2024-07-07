aee857e4f35d4b4eb268501f1efabfdd1d2c6b3a
Michael Winding <michael.winding@crick.ac.uk>
2024-07-07 22:21:11 +0100

    added parallel processing per video

--- sbatch-files/sleap-convert_slp.py
+++ sbatch-files/sleap-convert_slp.py
@@ -4,6 +4,7 @@ import pyarrow.feather as feather
 import pandas as pd
 import numpy as np
 from sleap.io.format import read
+from concurrent.futures import ProcessPoolExecutor
 
 # pulling user-input variables from command line
 parser = argparse.ArgumentParser(description='sleap_inference: convert .slp files to .feather on NEMO')
@@ -22,23 +23,28 @@ if model == 'topdown': skel_parts = ['head', 'body', 'tail']
 if model == 'pupae': skel_parts = ['head', 'body', 'tail']
 
 # convert .slp to .feather
-def slp_to_feather(file_path, skel_parts):
+def process_frame(frame, skel_parts):
+    data = []
+    for j, instance in enumerate(frame._instances):
+        array = [j] + [frame.frame_idx] + list(instance.points_and_scores_array.flatten())
+        array = [np.round(x, 2).astype('float32') for x in array] # reduce size of data
+        data.append(array)
+    return data
 
+def slp_to_feather(file_path, skel_parts):
     feather_file = file_path.replace('.slp', '.feather')
     label_obj = read(file_path, for_object='labels')
 
-    data = []
-    for i, frame in enumerate(label_obj.labeled_frames):
-        for j, instance in enumerate(frame._instances):
-            array = [j] + [i] + list(instance.points_and_scores_array.flatten())
-            array = [np.round(x, 2).astype('float32') for x in array] # reduce size of data
-            data.append(array)
+    with ProcessPoolExecutor() as executor:
+        results = executor.map(process_frame, label_obj.labeled_frames, [skel_parts]*len(label_obj.labeled_frames))
+
+    data = [item for sublist in results for item in sublist]
 
     columns = ['track_id', 'frame']
     for part in skel_parts:
         columns.extend([f'x_{part}', f'y_{part}', f'score_{part}'])
 
-    df = pd.DataFrame(data, columns = columns)
+    df = pd.DataFrame(data, columns=columns)
     df.to_feather(feather_file)
 
 # identify paths and filenames of all .mp4s in folder
@@ -47,7 +53,8 @@ if(os.path.isdir(videos_path)):
 else:
     print('Error: -p/--videos-path is not a directory!')
 
-video_file_paths = [x.replace('.mp4', '.predictions.slp') for x in video_file_paths]
+video_file_paths = [x.replace('.mp4', '.predictions.slp')]
 
 for path in video_file_paths:
     slp_to_feather(path, skel_parts)
+
