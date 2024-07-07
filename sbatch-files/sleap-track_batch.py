534007d38bb0649c78d2d77438c0cd0224bd0f5a
Michael Winding <michael.winding@crick.ac.uk>
2024-07-07 21:10:58 +0100

    minor change

--- sbatch-files/sleap-track_batch.py
+++ sbatch-files/sleap-track_batch.py
@@ -202,8 +202,6 @@ ml purge
 ml Anaconda3/2023.09-0
 source /camp/apps/eb/software/Anaconda/conda.env.sh
 
-source activate sleap
-
 # Debugging information
 echo "SLURM job id: $SLURM_JOB_ID"
 echo "SLURM task id: $SLURM_ARRAY_TASK_ID"
@@ -225,8 +223,8 @@ echo "Processing slp: $name_var.predictions.slp"
 echo "Full path to slp: {videos_path}.predictions.slp"
 echo "Output path: {videos_path}/$name_var.predictions.feather"
 
-cmd="python3 -u /camp/lab/windingm/home/shared/TestDev/Crick-HPC-files/sbatch-files/sleap-convert_slp.py -p "{videos_path}/$name_var.predictions.slp" -m "{model}"" 
-echo $cmd
+conda activate sleap
+cmd="python -u /camp/lab/windingm/home/shared/TestDev/Crick-HPC-files/sbatch-files/sleap-convert_slp.py -p "{videos_path}/$name_var.predictions.slp" -m "{model}"" 
 eval $cmd > python_output_convert-slp.log 2>&1
 """
 
