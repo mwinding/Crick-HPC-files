e8f9c7bc4c092b7d968d816a839b1f35ce0555f9
Michael Winding <michael.winding@crick.ac.uk>
2024-07-07 22:40:01 +0100

    simplified process, but parallelised file processing

--- sbatch-files/sleap-convert_slp.py
+++ sbatch-files/sleap-convert_slp.py
@@ -22,58 +22,35 @@ if model == 'sideview': skel_parts = ['head', 'mouthhooks', 'body', 'tail', 'spi
 if model == 'topdown': skel_parts = ['head', 'body', 'tail']
 if model == 'pupae': skel_parts = ['head', 'body', 'tail']
 
-# Extracting data from frames for parallel processing
-def extract_frame_data(frame):
-    frame_idx = frame.frame_idx
-    instances_data = []
-    for instance in frame._instances:
-        instance_data = list(instance.points_and_scores_array.flatten())
-        instances_data.append(instance_data)
-    return frame_idx, instances_data
-
-# Processing each frame's data
-def process_frame_data(frame_idx, instances_data, skel_parts):
-    data = []
-    for j, instance_data in enumerate(instances_data):
-        array = [j, frame_idx] + instance_data
-        array = [np.round(x, 2).astype('float32') for x in array]  # reduce size of data
-        data.append(array)
-    return data
-
-# Convert .slp to .feather
+# convert .slp to .feather
 def slp_to_feather(file_path, skel_parts):
+
     feather_file = file_path.replace('.slp', '.feather')
     label_obj = read(file_path, for_object='labels')
 
-    # Extract data from frames
-    frame_data = [extract_frame_data(frame) for frame in label_obj.labeled_frames]
-
-    # Process data in parallel
-    results = Parallel(n_jobs=-1)(
-        delayed(process_frame_data)(frame_idx, instances_data, skel_parts)
-        for frame_idx, instances_data in frame_data
-    )
-
-    data = [item for sublist in results for item in sublist]
+    data = []
+    for i, frame in enumerate(label_obj.labeled_frames):
+        for j, instance in enumerate(frame._instances):
+            array = [j] + [i] + list(instance.points_and_scores_array.flatten())
+            array = [np.round(x, 2).astype('float32') for x in array] # reduce size of data
+            data.append(array)
 
     columns = ['track_id', 'frame']
     for part in skel_parts:
         columns.extend([f'x_{part}', f'y_{part}', f'score_{part}'])
 
-    df = pd.DataFrame(data, columns=columns)
+    df = pd.DataFrame(data, columns = columns)
     df.to_feather(feather_file)
 
-# Identify paths and filenames of all .mp4s in folder
-if os.path.isdir(videos_path):
-    video_file_paths = [
-        f'{videos_path}/{f}' for f in os.listdir(videos_path)
-        if os.path.isfile(os.path.join(videos_path, f)) and f.endswith('.mp4')
-    ]
+# identify paths and filenames of all .mp4s in folder
+if(os.path.isdir(videos_path)):
+    video_file_paths = [f'{videos_path}/{f}' for f in os.listdir(videos_path) if os.path.isfile(os.path.join(videos_path, f)) and (f.endswith('.mp4'))]
 else:
     print('Error: -p/--videos-path is not a directory!')
 
-video_file_paths = [x.replace('.mp4', '.predictions.slp') for x in video_file_paths]
+video_file_paths = [x.replace('.mp4', '.predictions.slp')]
 
-for path in video_file_paths:
-    slp_to_feather(path, skel_parts)
-
+# Parallelize the conversion of .slp files to .feather files
+Parallel(n_jobs=-1)(
+    delayed(slp_to_feather)(path, skel_parts) for path in video_file_paths
+)
